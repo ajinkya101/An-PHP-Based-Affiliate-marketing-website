b31b5409efa29bf3a8f0337219be44f3f01a9f45
Ajinkya Bhabal <bhabalajinkya@gmail.com>
2018-05-21 10:20:27 +0530

    Home page

---
+++ index.php
@@ -0,0 +1,171 @@
+﻿
+<?php
+
+include("functions/functions.php");
+include("includes/db.php");
+
+cart();
+?>
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<?php
+
+include("header.php");
+
+?>
+
+
+    <!-- end of menu tab -->
+    <div class="crumb_navigation"> Navigation: <span class="current">Home</span>
+    
+
+    <?php
+    
+    if(isset($_GET['cat'])){
+    
+    $get_id = $_GET['cat'];
+    
+    $query = "select cat_title from categories where cat_id='$get_id'";
+    $run_query = mysqli_query($con,$query);
+    
+    $row = mysqli_fetch_array($run_query);
+    
+    $cat = $row['cat_title'];
+    
+    echo"<span class=current>>$cat</span>";
+    
+    }
+    
+    ?>
+
+ </div>
+    <div class="left_content">
+      <div class="title_box">Categories</div>
+      <ul class="left_menu">
+        
+ <?php 
+     getcats(); 
+     
+
+ ?>
+        
+      </ul>
+     
+<div class="title_box">Manufacturers</div>
+      <ul class="left_menu">
+        
+
+    <?php  getbrands();  ?>
+
+
+ 
+</ul>
+ <br>
+ <br>
+
+     
+      <div class="banner_adds"> <a href="#"><img src="images/bann2.jpg" alt="" border="0" /></a> </div>
+      
+           <br>
+ <br>
+ <br>
+     
+ 
+        <?php
+
+         special();
+
+        ?>
+
+    </div>
+    <!-- end of left content -->
+    <div class="center_content">
+      <div class="center_title_bar">Latest Products</div>
+
+
+<?php  
+
+getpro();
+get_pro_cat();
+get_pro_brand();
+
+ ?>
+
+
+
+
+ <div class="center_title_bar">Recommended Products</div>
+  
+  
+ <?php  getpro3(); ?>
+
+
+  </div>
+  
+ 
+  
+  
+ 
+    <!-- end of center content -->
+<div class="right_content">
+      <div class="shopping_cart">
+        <div class="cart_title"><i>Welcome Guest!</i><br>Shopping cart</div>
+        <div class="cart_details"><font color="blue"><?php total_items(); ?></font>&nbsp;item(s) <br />
+          <span class="border_cart"></span> Total:<span class="price"><?php total_price(); ?> </span> </div>
+        <div class="cart_icon"><a href="cart.php" title="header=[Checkout] body=[&nbsp;] fade=[on]"><img src="images/shoppingcart.png" alt="" width="48" height="48" border="0" /></a></div>
+      </div>
+      
+       
+       <br>
+     
+            <?php
+ 
+            special();
+
+           ?> 
+
+        <br>
+     
+ 
+        <?php
+
+         special();
+
+        ?>
+        
+             <br>
+     
+ 
+        <?php
+
+         special();
+
+        ?>
+     <br>
+     
+ 
+        <?php
+
+         special();
+
+        ?>
+
+
+      
+        <!-- end of right content -->
+</div>
+  <!-- end of main content -->
+<?php
+
+include("footer.php");
+
+?>
+
+</div>
+<!-- end of main_container -->
+</body>
+</html>
